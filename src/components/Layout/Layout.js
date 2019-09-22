d66ebfb69c7420cf9d17b4e33bd6da2d0baf9995
Ed <leon.eduardo111@gmail.com>
2019-09-21 21:02:16 -0400

    Adding Sidedrawer , the ToggleSidedrawer and the Logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Aux from '../../hoc/Aux';
 import styles from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
+const Layout = props => {
+    const [showSideDrawerState, setShowSideDrawerState] = useState(false);
 
-const layout = props => {
+    const sideDrawerClosedHandler = () => {
+        setShowSideDrawerState(false);
+    }
+    const sideDrawerHandler = () => {
+        setShowSideDrawerState((prevState) => (!showSideDrawerState))
+    }
     return (
         <Aux>
-            <Toolbar />
-            <div>
-                ToolBar , SideBar , Backdrop
-            </div>
+            <Toolbar clicked={sideDrawerHandler} />
+            <SideDrawer closed={sideDrawerClosedHandler} open={showSideDrawerState} />
             <main className={styles.Content}>{props.children}</main>
         </Aux>
 
     )
 }
 
-export default layout;
+export default Layout;
 
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import styles from './Toolbar.module.css';
+import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 const toolbar = props => {
     return (
         <header className={styles.Toolbar}>
-            <div>Menu</div>
-            <div>LOGO</div>
-            <nav>...</nav>
+            <DrawerToggle clicked={props.clicked} ></DrawerToggle>
+            <div className={styles.Logo}>
+                <Logo />
+            </div>
+            <nav className={styles.DesktopOnly}><NavigationItems /></nav>
 
         </header>
 
